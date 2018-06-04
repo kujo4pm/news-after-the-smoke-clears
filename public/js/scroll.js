eace4606db463e7759c77cef2636eb8951040fcf
Kurt Johnson <kurt.johnson@becollective.com>
2018-06-04 20:26:14 +1000

    Now have captions that fade in

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,24 +1,13 @@
-$(document).ready(function () {
-    $(window).on('load scroll', function () {
-        var scrolled = $(this).scrollTop();
-        $('#title').css({
-            'transform': 'translate3d(0, ' + -(scrolled * 0.2) + 'px, 0)', // parallax (20% scroll rate)
-            'opacity': 1 - scrolled / 400 // fade out at 400px from top
-        });
-        $('#hero-vid').css('transform', 'translate3d(0, ' + -(scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
-    });
-    
-    // video controls
-    $('#state').on('click', function () {
-        var video = $('#hero-vid').get(0);
-        var icons = $('#state > span');
-        $('#overlay').toggleClass('fade');
-        if (video.paused) {
-            video.play();
-            icons.removeClass('fa-play').addClass('fa-pause');
-        } else {
-            video.pause();
-            icons.removeClass('fa-pause').addClass('fa-play');
-        }
-    });
+$(document).ready(function() {
+  /* Every time the window is scrolled ... */
+  $(window).scroll(function() {
+    $('figcaption').each(function(i) {
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + window.innerHeight;
+      /* If the object is completely visible in the window, fade it it */
+      if(bottom_of_window > bottom_of_object) {
+        $(this).animate({ 'opacity':'1' }, 500);
+      }
+    }); 
+  });
 });
