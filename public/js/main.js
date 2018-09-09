02f9f8f8040786a60d3bc4a873603af461fc81fe
Kurt Johnson <kujo4pm@gmail.com>
2018-09-09 17:19:15 +1000

    Created an audio player

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,24 @@ $('#share-link').on('click',
     $('#share-panel').toggleClass('hidden');
   }
 );
+
+$(
+  () => {
+  let aud = $('audio')[0];
+  $('#play-pause').on('click',  () => {
+    if (aud.paused) {
+      aud.play();
+      $('#play-pause').removeClass('fa-play-circle');
+      $('#play-pause').addClass('fa-pause-circle');
+    }
+    else {
+      aud.pause();
+      $('#play-pause').removeClass('fa-pause-circle');
+      $('#play-pause').addClass('fa-play-circle');
+    }
+  
+})
+  aud.ontimeupdate = () => {
+    $('.progress').css('width', aud.currentTime / aud.duration * 100 + '%')
+  }
+})
